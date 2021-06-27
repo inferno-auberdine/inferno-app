02851196b1a6dc9c8e4a4d6bfdc9f3b59782d405
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-06-27 04:06:23 +0200

    fix: corrige une erreur de type dans la commande de build et supprime le dossier inferno-app du chemin de build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/inferno-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
