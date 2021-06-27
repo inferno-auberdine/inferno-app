7dbb420ced6455669c9ef69243044416f915051e
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-06-27 04:14:22 +0200

    fix: corrige les erreurs du script server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
