709e1ff8e74f5b17f54878801ae1d4c8652ed3e3
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-05 20:56:35 +0200

    feat(raid): remplace les donées en dur par des appels à l'api
     * mise en place du proxy pour les appels en local
     * mise en place de la classe Uris pour le stockage des uris
     * mise e place des environnements

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './components/header/header.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ]
 })
 export class SharedModule {
